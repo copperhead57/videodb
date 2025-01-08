5df125c0ee28c32290ee0a41b20ba2c3f372ed1f
Paul copperhead <copperhead_57@hotmail.com>
2025-01-08 14:33:42 +1100

    consolidate commits in code-v2 (#91)
    * tested browse page for all version
    not pdb,xls,xml rss

--- core/functions.core.php
+++ core/functions.core.php
@@ -76,7 +76,11 @@ function dlog($var)
  */
 function formvar($name)
 {
+    if (!is_null($name))
+    {
 	return htmlspecialchars($name);
+    }
+    return "";
 }
 
 /**
@@ -124,9 +128,14 @@ function escapeSQL($sql_string)
 {
     global $dbh;
 
-    if (!is_resource($dbh)) $dbh = getConnection();
+    if (!is_null($sql_string))
+    {
+        if (!is_resource($dbh)) $dbh = getConnection();
+    
+        return(mysqli_real_escape_string($dbh, $sql_string));
+    }
 
-    return(mysqli_real_escape_string($dbh, $sql_string));
+    return ("");
 }
 
 /**
